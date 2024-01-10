6cef57402bf1b687b810cd870f8a2a1596a9ad98
Meshan Khosla <mkhosla@berkeley.edu>
2024-01-09 19:46:57 -0800

    Add async test

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -1,7 +1,9 @@
 import pytest
 import dotenv
 import time
+import asyncio
 from upstash_qstash import Client
+from upstash_qstash.asyncio import Client as AsyncClient
 from upstash_qstash.error import QstashException
 
 QSTASH_TOKEN = dotenv.dotenv_values()["QSTASH_TOKEN"]
@@ -29,6 +31,39 @@ def test_publish_to_url(client):
     time.sleep(1)
 
     events = client.events()
+    found_event = None
+    for event in events["events"]:
+        if event["messageId"] == res["messageId"]:
+            found_event = event
+            break
+
+    assert (
+        found_event
+    ), f"Event with messageId {res['messageId']} not found  This may be because the latency of the event is too high"
+    assert (
+        event["state"] != "ERROR"
+    ), f"Event with messageId {res['messageId']} was not delivered"
+
+
+@pytest.mark.asyncio
+async def test_publish_to_url_async():
+    client = AsyncClient(QSTASH_TOKEN)
+    res = await client.publish_json(
+        {
+            "body": {"ex_key": "ex_value"},
+            "url": "https://example.com",
+            "headers": {
+                "test-header": "test-value",
+            },
+        }
+    )
+
+    assert res["messageId"] is not None
+
+    print("Waiting 1 second for event to be delivered")
+    await asyncio.sleep(1)
+
+    events = await client.events()
     found_event = None
     for event in events["events"]:
         if event["messageId"] == res["messageId"]:
