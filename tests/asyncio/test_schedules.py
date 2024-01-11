88eba629abe3c972d609ddd1b5856c858f60a770
Meshan Khosla <mkhosla@berkeley.edu>
2024-01-10 19:02:24 -0800

    Add asyncio schedules test

--- tests/asyncio/test_schedules.py
+++ tests/asyncio/test_schedules.py
@@ -1,6 +1,6 @@
 import pytest
-import time
-from upstash_qstash import Client
+import asyncio
+from upstash_qstash.asyncio import Client
 from qstash_token import QSTASH_TOKEN
 
 
@@ -9,9 +9,10 @@ def client():
     return Client(QSTASH_TOKEN)
 
 
-def test_schedule_lifecycle(client):
+@pytest.mark.asyncio
+async def test_schedule_lifecycle(client):
     sched = client.schedules()
-    create_res = sched.create(
+    create_res = await sched.create(
         {
             "cron": "* * * * *",
             "destination": "https://example.com",
@@ -26,23 +27,23 @@ def test_schedule_lifecycle(client):
     assert sched_id is not None
 
     print("Waiting 1 seconds for schedule to be delivered")
-    time.sleep(1)
+    await asyncio.sleep(1)
 
     print("Checking if schedule is delivered")
-    get_res = sched.get(sched_id)
+    get_res = await sched.get(sched_id)
     assert get_res["scheduleId"] == sched_id
     assert get_res["cron"] == "* * * * *"
 
     print("Checking if schedule is in list")
-    list_res = sched.list()
+    list_res = await sched.list()
     assert any(s["scheduleId"] == sched_id for s in list_res)
 
     print("Deleting schedule")
-    sched.delete(sched_id)
+    await sched.delete(sched_id)
 
     print("Waiting 1 second for schedule to be deleted")
-    time.sleep(1)
+    await asyncio.sleep(1)
 
     print("Checking if schedule is deleted")
-    list_res = sched.list()
+    list_res = await sched.list()
     assert not any(s["scheduleId"] == sched_id for s in list_res)
