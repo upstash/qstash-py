7329776d88b91525340610df8cdc8211ce3aee76
Meshan Khosla <meshan@upstash.com>
2024-03-19 09:17:07 -0700

    Implement bulk DLQ delete (#11)

--- tests/test_dlq.py
+++ tests/test_dlq.py
@@ -43,3 +43,32 @@ def test_dlq(client):
         filter(lambda msg: msg["messageId"] == msg_id, all_messages_after_delete)
     )
     assert len(msg_deleted) == 0
+
+def test_dlq_delete_many(client):
+    print("Publishing 5 messages to a failed endpoint")
+    msg_ids = []
+    for _ in range(5):
+        pub_res = client.publish_json({"url": "http://httpstat.us/404", "retries": 0})
+        msg_ids.append(pub_res["messageId"])
+    assert len(msg_ids) == 5
+  
+    print("Waiting 5 seconds for events to be delivered")
+    time.sleep(5)
+  
+    print("Checking if messages are in DLQ")
+    dlq = client.dlq()
+    all_messages = dlq.list_messages()["messages"]
+    msg_sent = list(filter(lambda msg: msg["messageId"] in msg_ids, all_messages))
+    assert len(msg_sent) == 5
+  
+    print("Deleting messages from DLQ")
+    dlq_ids = [msg["dlqId"] for msg in msg_sent]
+    dlq.deleteMany({"dlq_ids": dlq_ids})
+
+    print("Checking if messages are deleted from DLQ")
+    all_messages_after_delete = dlq.list_messages()["messages"]
+    msg_deleted = list(
+        filter(lambda msg: msg["messageId"] in msg_ids, all_messages_after_delete)
+    )
+    assert len(msg_deleted) == 0
+    assert len(all_messages_after_delete) == len(all_messages) - 5
