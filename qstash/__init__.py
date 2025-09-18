e28c6b37bb913573985ec51795512e52d196ca61
CahidArda <cahidardaooz@hotmail.com>
2025-09-18 12:26:55 +0300

    fix: bump version to 3.2.0

--- qstash/__init__.py
+++ qstash/__init__.py
@@ -2,5 +2,5 @@ from qstash.asyncio.client import AsyncQStash
 from qstash.client import QStash
 from qstash.receiver import Receiver
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 __all__ = ["QStash", "AsyncQStash", "Receiver"]
